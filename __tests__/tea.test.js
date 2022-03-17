e2291fbb4b05359738683574406ff2ffeb4eb7e5
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-03-17 09:41:44 -0700

    made a new set of test for the tea resource

---
+++ __tests__/tea.test.js
@@ -0,0 +1,55 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const Tea = require('../lib/models/Tea');
+
+describe('hand-of-resources routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('creates a tea object', async () => {
+    const expected = {
+      name: 'ban-cha',
+      origin: 'japan',
+    };
+    const { body } = await request(app).post('/api/v1/teas').send(expected);
+    expect(body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('get and array full  of all the teas', async () => {
+    const expected = await Tea.getAll();
+    const { body } = await request(app).get('/api/v1/teas');
+    expect(body).toEqual(expected);
+  });
+
+  it('get a tea based on a id', async () => {
+    const expected = await Tea.findById(1);
+    const { body } = await request(app).get(`/api/v1/teas/${expected.id}`);
+    expect(expected).toEqual(body);
+  });
+
+  it('update the tea by the id', async () => {
+    const expected = {
+      id: expect.any(String),
+      name: 'mint-tea',
+      origin: 'unknown',
+    };
+    const { body } = await request(app).patch('/api/v1/teas/1').send({
+      name: 'mint-tea',
+      origin: 'unknown',
+    });
+    expect(body).toEqual(expected);
+  });
+
+  it('it deletes tea by id', async () => {
+    const expected = await Tea.findById(1);
+    const { body } = await request(app).delete(`/api/v1/teas/${expected.id}`);
+    expect(expected).toEqual(body);
+  });
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -3,6 +3,7 @@
 DROP TABLE IF EXISTS profiles,
 books;
 
+--Profiles table
 CREATE TABLE profiles (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name TEXT NOT NULL,
@@ -14,6 +15,7 @@ INSERT INTO
 VALUES
     ('james', 'king of cats');
 
+-- Books table
 CREATE TABLE books (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     author TEXT NOT NULL,
@@ -28,3 +30,16 @@ VALUES
         'the emotinal scientist and the messy gramatical pronoun'
     ),
     ('kurt Vonegget', 'sirans of titan');
+
+--Tea table
+CREATE TABLE tea (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    origin TEXT NOT NULL
+);
+
+INSERT INTO
+    tea (name, origin)
+VALUES
+    ('Earlgrey', 'England'),
+    ('Greentea', 'Japan');
